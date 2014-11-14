7d73e7106ee0577665ae8c575dcec6c3553384d1
Peter <pwgraham91@gmail.com>
2014-11-13 19:48:19 -0800

    ready for fb

--- speed_date/models.py
+++ speed_date/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    # username is RTC's userID
+    # full name will be first_name + last_name and be RTC's userName
+
+    def __unicode__(self):
+        return u"{}".format(self.username)
--- speed_date/views.py
+++ speed_date/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, "index.html")
+
+
+def caller(request):
+    return render(request, "caller.html")
+
+
+def callee(request):
+    return render(request, "callee.html")
