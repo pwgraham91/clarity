ba47247fe4f0535eb9b72d25ad8101e88dcf78b2
Peter <pwgraham91@gmail.com>
2014-11-19 11:58:48 -0800

    test live

--- speed_date/TODOS.py
+++ speed_date/TODOS.py
@@ -1,10 +1,8 @@
 __author__ = 'GoldenGate'
 """
-15) Ask Randall why when I reload runnable it drops my database
-2) Nice Looking Front Page
-12) Don't let user be called while already in a call
 9) Clean up javascript by putting most of them in dedicated js files
 5) Only request access to webcam once
 1) Get user location and return closest user
 2) Put environmental variables on
+put border around chat
 """
